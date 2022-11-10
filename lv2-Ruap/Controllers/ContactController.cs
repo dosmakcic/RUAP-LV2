aae38472f3ce7d9808884f62c172b5e71b15d30b
dosmakcic <dorijan.osmakcic135@gmail.com>
2022-11-10 19:06:24 +0100

    Prvi zadatak

---
+++ lv2-Ruap/Controllers/ContactController.cs
@@ -0,0 +1,36 @@
+﻿using lv2_Ruap.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using lv2_Ruap.Services;
+
+namespace lv2_Ruap.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        } 
+   
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+            {
+        
+     };
+
+        }
+    }
+}
---
+++ lv2-Ruap/Services/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using lv2_Ruap.Models;
+
+namespace lv2_Ruap.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+            new Contact
+            {
+                Id = 1,
+                Name = "Glenn Block"
+            },
+            new Contact
+            {
+                Id = 2,
+                Name = "Dan Roth"
+            }
+            };
+        }
+    }
+}
